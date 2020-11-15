a6d42cfcb65a60e0c6b823457f7c6870b9351396
satoru <65771625+SatoruAkiyama@users.noreply.github.com>
2020-11-15 17:10:22 +0900

    fixed bug

--- components/DevicesTabs.js
+++ components/DevicesTabs.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
@@ -102,7 +103,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function DevicesTabs() {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
